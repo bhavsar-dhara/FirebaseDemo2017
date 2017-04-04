5337ca5dcf7b19e509dde0415a54ce0731cae437
Dhara Bhavsar <dharapb@ccs.neu.edu>
2017-04-03 23:12:25 -0400

    Removed unnecessary auth linking account code

--- app/src/main/java/edu/neu/madcourse/dharabhavsar/firebaserealtimedemo/AnonymousAuthActivity.java
+++ app/src/main/java/edu/neu/madcourse/dharabhavsar/firebaserealtimedemo/AnonymousAuthActivity.java
@@ -24,18 +24,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -58,9 +53,6 @@ public class AnonymousAuthActivity extends BaseActivity implements
     // [START declare_auth_listener]
     private FirebaseAuth.AuthStateListener mAuthListener;
     // [END declare_auth_listener]
-
-    private EditText mEmailField;
-    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,14 +97,9 @@ public class AnonymousAuthActivity extends BaseActivity implements
         };
         // [END auth_state_listener]
 
-        // Fields
-        mEmailField = (EditText) findViewById(R.id.field_email);
-        mPasswordField = (EditText) findViewById(R.id.field_password);
-
         // Click listeners
         findViewById(R.id.button_anonymous_sign_in).setOnClickListener(this);
         findViewById(R.id.button_anonymous_sign_out).setOnClickListener(this);
-        findViewById(R.id.button_link_account).setOnClickListener(this);
         findViewById(R.id.button_download_file).setOnClickListener(this);
         findViewById(R.id.button_download_file_2).setOnClickListener(this);
     }
@@ -166,87 +153,14 @@ public class AnonymousAuthActivity extends BaseActivity implements
         updateUI(null);
     }
 
-    private void linkAccount() {
-        // Make sure form is valid
-        if (!validateLinkForm()) {
-            return;
-        }
-
-        // Get email and password from form
-        String email = mEmailField.getText().toString();
-        String password = mPasswordField.getText().toString();
-
-        // Create EmailAuthCredential with email and password
-        AuthCredential credential = EmailAuthProvider.getCredential(email, password);
-
-        // Link the anonymous user to the email credential
-        showProgressDialog();
-        // [START link_credential]
-        mAuth.getCurrentUser().linkWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "linkWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(AnonymousAuthActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // [START_EXCLUDE]
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
-                    }
-                });
-        // [END link_credential]
-    }
-
-    private boolean validateLinkForm() {
-        boolean valid = true;
-
-        String email = mEmailField.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            mEmailField.setError("Required.");
-            valid = false;
-        } else {
-            mEmailField.setError(null);
-        }
-
-        String password = mPasswordField.getText().toString();
-        if (TextUtils.isEmpty(password)) {
-            mPasswordField.setError("Required.");
-            valid = false;
-        } else {
-            mPasswordField.setError(null);
-        }
-
-        return valid;
-    }
-
     private void updateUI(FirebaseUser user) {
         hideProgressDialog();
 
-        TextView idView = (TextView) findViewById(R.id.anonymous_status_id);
-        TextView emailView = (TextView) findViewById(R.id.anonymous_status_email);
         boolean isSignedIn = (user != null);
-
-        // Status text
-        if (isSignedIn) {
-            idView.setText(getString(R.string.id_fmt, user.getUid()));
-            emailView.setText(getString(R.string.email_fmt, user.getEmail()));
-//            downloadFile();
-        } else {
-            idView.setText(R.string.signed_out);
-            emailView.setText(null);
-        }
 
         // Button visibility
         findViewById(R.id.button_anonymous_sign_in).setEnabled(!isSignedIn);
         findViewById(R.id.button_anonymous_sign_out).setEnabled(isSignedIn);
-        findViewById(R.id.button_link_account).setEnabled(isSignedIn);
         findViewById(R.id.button_download_file).setEnabled(isSignedIn);
         findViewById(R.id.button_download_file_2).setEnabled(isSignedIn);
     }
@@ -258,8 +172,6 @@ public class AnonymousAuthActivity extends BaseActivity implements
             signInAnonymously();
         } else if (i == R.id.button_anonymous_sign_out) {
             signOut();
-        } else if (i == R.id.button_link_account) {
-            linkAccount();
         } else if (i == R.id.button_download_file) {
             openDownloadFileActivity();
         } else if (i == R.id.button_download_file_2) {
