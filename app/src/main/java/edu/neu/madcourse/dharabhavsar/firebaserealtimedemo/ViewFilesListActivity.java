b449310a3370b6a572bc54f3f05da5f81a18903b
Dhara Bhavsar <dharapb@ccs.neu.edu>
2017-04-11 19:57:07 -0400

    updated view file list activity

--- app/src/main/java/edu/neu/madcourse/dharabhavsar/firebaserealtimedemo/ViewFilesListActivity.java
+++ app/src/main/java/edu/neu/madcourse/dharabhavsar/firebaserealtimedemo/ViewFilesListActivity.java
@@ -69,8 +69,7 @@ public class ViewFilesListActivity extends AppCompatActivity implements AdapterV
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        adapter = new ArrayAdapter<>(ViewFilesListActivity.this,
-                android.R.layout.simple_spinner_item, fileList);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, fileList);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
